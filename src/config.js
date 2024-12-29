02e42d9d4ce14bf1a367662064b36fce86e643ef
Chethan <cchethans14@gamil.com>
2024-12-29 23:10:34 +0530

    29 Dec 2025

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-// const BASE_API_URL = process.env.BASE_API_URL;
-// console.log("base api url")
-// console.log(BASE_API_URL)
+const BASE_API_URL = process.env.BASE_API_URL;
+console.log("base api url")
+console.log(BASE_API_URL)
 
 // // config.js
-const BASE_API_URL =
-  "https://my-spring-boot-app-381139703931.us-central1.run.app";
+//const BASE_API_URL =
+ // "https://my-spring-boot-app-381139703931.us-central1.run.app";
 
 // const BASE_API_URL = "http://localhost:8080";
 export default BASE_API_URL;
